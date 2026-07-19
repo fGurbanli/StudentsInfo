9623f5eb5ba57e7fec700c1e82eb8749b08080a0
Furgan Gurbanli <f.qurbanli.2006@gmail.com>
2026-07-19 11:58:52 +0400

    Printing Menu

--- main.c
+++ main.c
@@ -15,10 +15,19 @@ void SearchStudent();
 
 int main(void) {
 
-
-
+    PrintMenu();
 
     return 0;
+}
+
+void PrintMenu()
+{
+    printf("\n=====Student Information System=====\n\n");
+    printf("1- List of Student\n");
+    printf("2- Add a Student\n");
+    printf("3- Search a Student\n");
+    printf("4- Delete a Student\n");
+    printf("\n0- Exit\n");
 }
 
 int GetIntInput() {
